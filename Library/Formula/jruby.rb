210d2ac9da5fa4fb65fad25105753551e1c4c564
Clinton R. Nixon <crnixon@gmail.com>
2009-11-11 16:09:59 -0500

    updated JRuby formula

--- Library/Formula/jruby.rb
+++ Library/Formula/jruby.rb
@@ -4,9 +4,9 @@ require 'formula'
 # TODO trim that a bit? Seems crazy.
 
 class Jruby <Formula
-  @url='http://dist.codehaus.org/jruby/1.3.1/jruby-src-1.3.1.tar.gz'
-  @homepage='http://jruby.org/'
-  @md5='c7e2aa4a3065db445a8b3e17ecff9fe0'
+  url 'http://jruby.kenai.com/downloads/1.4.0/jruby-src-1.4.0.tar.gz'
+  homepage 'http://jruby.org/'
+  md5 'a363b6c2ea24f0ef8df478c93ac8cc59'
 
   def install
     system "ant"
